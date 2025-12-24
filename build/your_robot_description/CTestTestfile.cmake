0fa6fd3b2e74d36ed4395c595d88f21f5cbff105
Yujin <112687084+YujinCIX@users.noreply.github.com>
2025-12-24 16:46:10 +0300

    commit_1

---
+++ build/your_robot_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/sava/robotics/ros2_ws/src/your_robot_description
+# Build directory: /home/sava/robotics/ros2_ws/build/your_robot_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
